dc4788cb8416753c52bbd716003b8188a2764437
ayaanyousaf <ayaanyousaf@hotmail.com>
2025-12-02 23:21:23 -0500

    connect frontend by serving /public via express

--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.send("Game Patch Notes AI server is running...");
 });
